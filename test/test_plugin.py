d67313a6659c4a81236ea7ac9c48203c8f95b46d
Inderjeet Singh <sethigoldy@gmail.com>
2026-08-31 10:14:05 +0530

    Release 0.2.3

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -422,7 +422,7 @@ class Version(unittest.TestCase):
     stopped guarding.
     """
 
-    VERSION = "0.2.2"
+    VERSION = "0.2.3"
     DECLARED = {
         '.grok-plugin/marketplace.json',
         'plugin.json',
